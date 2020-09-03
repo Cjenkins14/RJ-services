c6eb25e2aa607a561db4a984b8781a1a89a72be7
cjenkins14 <coreyjenkins24@gmail.com>
2020-09-02 21:06:20 -0500

    final version

--- src/Gallery/photos.js
+++ src/Gallery/photos.js
@@ -1,13 +1,13 @@
 const photos = [
     {
         src: 'https://user-images.githubusercontent.com/54726437/91078961-0dde7b80-e609-11ea-9575-d3bcc3e276cc.png',
-        width: 3,
-        height: 4
+        width: 4,
+        height: 5
     },
     {
         src: 'https://user-images.githubusercontent.com/54726437/91366431-52187a00-e7c9-11ea-8d48-13258b37a038.png',
-        width: 1,
-        height: 1
+        width: 2,
+        height: 2
     },
     {
         src: 'https://user-images.githubusercontent.com/54726437/91090577-ee504e80-e61a-11ea-8952-1da9190ac4e0.png',
